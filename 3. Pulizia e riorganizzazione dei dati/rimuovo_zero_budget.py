d8df600a45043c7b2087e27a864699782238e977
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-24 09:24:50 +0200

    Add files via upload
    fixed problema budget e revenue

--- 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
+++ 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
@@ -1,26 +1,29 @@
 import pandas as pd
 
-df=pd.read_csv("C://Users//Loredana//Desktop//Academy MM//PYTHON//PROGETTO//movies_metadata_CLEAN_ID.csv",low_memory=False)
+df=pd.read_csv('C:\\Users\\Utilizzatore\\Desktop\\Academy\\ML Project\\Movies Dataset\\movies_metadata_CLEAN_ID.csv',low_memory=False)
 print(df.head())
 print(df.info())  # Vedi il tipo di dati
 print(df.describe())  # Statistiche di base
 
 
-#rimuovo osservazioni con budget 0
+
+# Converti 'budget' e 'revenue' in valori numerici (float), rimuovendo eventuali errori
 df['budget'] = pd.to_numeric(df['budget'], errors='coerce')
+df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce')
+
+# Rimuovi righe con budget pari a 0
 df_cleaned = df[df['budget'] != 0]
 
-#rimuovo osservazioni con revenue 0
-df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce')
-df_cleaned = df[df['revenue'] != 0]
+# Rimuovi righe con revenue pari a 0 (su df_cleaned, non su df originale)
+df_cleaned = df_cleaned[df_cleaned['revenue'] != 0]
 
 #rimuovo titoli con numeri
-titoli_giu = df['title'].str.isalpha()
-df_cleaned = df[titoli_giu]
+titoli_giu = df_cleaned['title'].str.isalpha()
+df_cleaned = df_cleaned[titoli_giu]
 
 
 # Salva il file CSV pulito
-df_cleaned.to_csv('C://Users//Loredana//Desktop//Academy MM//PYTHON//PROGETTO//movies_metadata_CLEAN_id_budget.csv', index=False)
+df_cleaned.to_csv('C:\\Users\\Utilizzatore\\Desktop\\Academy\\ML Project\\Movies Dataset\\movies_metadata_CLEAN_ID_budget.csv', index=False)
 
 print(df_cleaned.head())
 print(df_cleaned.info())  # Vedi il tipo di dati

b753255706a9cb29191230b9ede38cccdc6f5c2c
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-24 09:52:18 +0200

    Add files via upload
    aggiunto controllo anche sui nomi delle compagnie di produzione

--- 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
+++ 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
@@ -1,12 +1,22 @@
 import pandas as pd
+import ast
 
+def contiene_numeri(s):
+    return any(char.isdigit() for char in s)
 # Carica il dataset
 df = pd.read_csv('C:\\Users\\Utilizzatore\\Desktop\\Academy\\ML Project\\Movies Dataset\\movies_metadata_CLEAN_ID.csv', low_memory=False)
-
+# Funzione per controllare se una stringa contiene numeri
 # Mostra informazioni di base sul dataset
 print(df.head())
 print(df.info())  # Vedi il tipo di dati
 print(df.describe())  # Statistiche di base
+
+# Se la colonna 'production_company' è in formato stringa di lista, convertila in lista Python
+df['production_companies_list'] = df['production_companies_list'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+
+# Filtra il dataframe per rimuovere le righe con nomi di aziende che contengono numeri
+df_cleaned = df[~df['production_companies_list'].apply(lambda companies: any(contiene_numeri(company) for company in companies))]
+
 
 # Filtra i titoli che NON contengono numeri (rimuovi titoli con numeri come 'se7en')
 df_cleaned = df[~df['title'].str.contains(r'\d', regex=True, na=False)]

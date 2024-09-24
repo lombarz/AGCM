203b641862fcfd686ea7c9642dce05337b1a99c3
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-24 09:34:59 +0200

    Add files via upload
    risolto problema titoli con spazi

--- 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
+++ 3. Pulizia e riorganizzazione dei dati/rimuovo_zero_budget.py
@@ -5,7 +5,9 @@ print(df.head())
 print(df.info())  # Vedi il tipo di dati
 print(df.describe())  # Statistiche di base
 
-
+#rimuovo titoli con numeri
+# Filtra i titoli che contengono solo caratteri alfabetici, spazi e segni di punteggiatura
+df_cleaned = df[df['title'].str.contains(r'^[^\d]+$', regex=True, na=False)]
 
 # Converti 'budget' e 'revenue' in valori numerici (float), rimuovendo eventuali errori
 df['budget'] = pd.to_numeric(df['budget'], errors='coerce')
@@ -17,9 +19,7 @@ df_cleaned = df[df['budget'] != 0]
 # Rimuovi righe con revenue pari a 0 (su df_cleaned, non su df originale)
 df_cleaned = df_cleaned[df_cleaned['revenue'] != 0]
 
-#rimuovo titoli con numeri
-titoli_giu = df_cleaned['title'].str.isalpha()
-df_cleaned = df_cleaned[titoli_giu]
+
 
 
 # Salva il file CSV pulito
